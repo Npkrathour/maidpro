2780bfa83f1169d7c433917c04e23eb5120252eb
Pinku rathour <pinkurathour2016@gmail.com>
2024-09-06 23:38:16 +0530

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,45 @@
+AOS.init();
+// Counter Number
+$(".number").counterUp({ time: 1000 });
+
+
+// modal slider
+$('.owl-slider-1').owlCarousel({
+    loop:true,
+    margin:10,
+    nav: true,
+    infinite: false,
+    autoPlay: true,
+    navigation : true,
+    navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+// ============
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
